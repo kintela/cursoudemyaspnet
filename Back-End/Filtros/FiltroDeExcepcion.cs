3f0785e8919ac6bce3a9542f45e64baf59c31217
Kintela <roberto.quintela@gmail.com>
2021-09-30 16:10:08 +0200

    filttos y demas

---
+++ Back-End/Filtros/FiltroDeExcepcion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace Back_End.Filtros
+{
+  public class FiltroDeExcepcion:ExceptionFilterAttribute
+  {
+    private readonly ILogger<FieldAccessException> logger;
+
+    public FiltroDeExcepcion(ILogger<FieldAccessException> logger)
+    {
+      this.logger = logger;
+    }
+
+    public override void OnException(ExceptionContext context)
+    {
+      logger.LogError(context.Exception, context.Exception.Message);
+      base.OnException(context);
+    }
+  }
+}
